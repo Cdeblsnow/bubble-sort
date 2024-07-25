3dbc1e49560763ac1aae1963e4a759174995eb00
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-07-24 23:21:47 -0400

    Finish bubble_sort logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,28 @@
 puts "Please enter the numbers you are looking to sort"
 number_array = gets.chomp.split(",")
 number_array = number_array.map{|number| number.to_i}
-comparative_array = number_array.sort
-length = number_array.length
+
 
 def bubble_sort(array)
+sorted_array = number_array
+accumulator = 0
 
   (array.length - 1).times do |counter|
-    p counter
-  end  
+    
+    (array.length - accumulator).times do |iteration|
+      number = array[iteration]
+      next_number = array[iteration+1]
+      if number > next_number
+        sorted_array[number] = sorted_array[next_number]  
+        sorted_array[next_number]= sorted_array[number]
+      end
+      accumulator += 1
+    end
+    if sorted_array = array.sort
+      break
+    end
+  end
+  p sorted_array  
 end
 
 bubble_sort(number_array)
