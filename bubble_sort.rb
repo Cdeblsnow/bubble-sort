f95a21958c4eeb2beae30f4c640bc54fb3b010ce
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-07-24 22:55:11 -0400

    Initialize the method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,10 +2,13 @@ puts "Please enter the numbers you are looking to sort"
 number_array = gets.chomp.split(",")
 number_array = number_array.map{|number| number.to_i}
 comparative_array = number_array.sort
-
+length = number_array.length
 
 def bubble_sort(array)
 
+  (array.length - 1).times do |counter|
+    p counter
+  end  
 end
 
 bubble_sort(number_array)
